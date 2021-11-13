eeb4741a2e743a9e0d06019a2375f45871db3476
sagnitude <leyicong@gmail.com>
2021-11-13 11:16:14 +0800

    init project

---
+++ xiake.py
@@ -0,0 +1,17 @@
+from hoshino import Service
+
+@sv.on_prefix("开启活动提醒")
+async def remind_enable_hatsune(bot, ev):
+    gid = str(ev['group_id'])
+
+@sv.on_prefix("关闭活动提醒")
+async def remind_enable_hatsune(bot, ev):
+    gid = str(ev['group_id'])
+
+@sv.on_prefix("开启卡池提醒")
+async def remind_enable_hatsune(bot, ev):
+    gid = str(ev['group_id'])
+
+@sv.on_prefix("关闭活动提醒")
+async def remind_enable_hatsune(bot, ev):
+    gid = str(ev['group_id'])
